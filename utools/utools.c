6fb2e4733ba5706341e4750838458b02e263f867
IAKSH <1248675212@qq.com>
2022-09-29 17:56:34 +0800

    fix the bug of function "squareCollisionCheck"

--- utools/utools.c
+++ utools/utools.c
@@ -34,6 +34,25 @@ static int getFileNameIndex(const char* path)
 
 bool squareCollisionCheck(int x1,int y1,int w1,int h1,int x2,int y2,int w2,int h2)
 {
+    if(w1 > w2 || h1 > h2)
+    {
+        int buffer;
+        buffer = w2;
+        w2 = w1;
+        w1 = buffer;
+
+        buffer = h2;
+        h2 = h1;
+        h1 = buffer;
+
+        buffer = x2;
+        x2 = x1;
+        x1 = buffer;
+
+        buffer = y2;
+        y2 = y1;
+        y1 = buffer;
+    }
     // Linear programming
     int vertex[4][2] = {{x1,y1},{x1,y1+h1},{x1+w1,y1},{x1+w1,y1+h1}};
     for(int i = 0;i < 4;i++)
