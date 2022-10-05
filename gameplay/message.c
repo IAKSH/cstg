a37e4645dd885a44f2385c253e56003194f2726f
IAKSH <1248675212@qq.com>
2022-10-05 22:18:59 +0800

    use libutools's linklist on message.c

--- gameplay/message.c
+++ gameplay/message.c
@@ -1,61 +1,22 @@
 #include "message.h"
+#include <linklist.h>
 #include <stdlib.h>
 
-// Link List of Msg
-typedef struct MsgLinkListNode {
-    GamePlayMsg             msg;
-    struct MsgLinkListNode* next;
-} MsgLinkListNode_t;
-
-typedef struct {
-    MsgLinkListNode_t* first;
-} MsgLinkListHead;
-
 // invisibel from outside
-static MsgLinkListHead globalMsgs;
+static LinkListHead_t globalMsgs;
 
-void msgsInit(void) { globalMsgs.first = NULL; }
+void msgsInit(void) {linkListInitialize(&globalMsgs);};
 
-void msgsAdd(GamePlayMsg* msg)
-{
-    if(globalMsgs.first == NULL)
-    {
-        globalMsgs.first       = (MsgLinkListNode_t*)malloc(sizeof(MsgLinkListNode_t));
-        globalMsgs.first->next = NULL;
-        // copy
-        globalMsgs.first->msg = *msg;
-    }
-    else
-    {
-        MsgLinkListNode_t* node = globalMsgs.first;
-        while(node->next != NULL) node = node->next;
-        node->next = (MsgLinkListNode_t*)malloc(sizeof(MsgLinkListNode_t));
-        node       = node->next;
-        node->next = NULL;
-        // copy
-        node->msg = *msg;
-    }
-}
+void msgsAdd(GamePlayMsg* msg) { linkListInsertTail(&globalMsgs, msg,sizeof(*msg)); }
 
 void msgsForeach(void (*func)(GamePlayMsg*))
 {
-    MsgLinkListNode_t* node = globalMsgs.first;
+    LinkListNode_t* node = globalMsgs.first;
     while(node != NULL)
     {
-        func(&node->msg);
+        func(node->var);
         node = node->next;
     }
 }
 
-void msgsClean(void)
-{
-    MsgLinkListNode_t* node = globalMsgs.first;
-    MsgLinkListNode_t* preNode;
-    while(node != NULL)
-    {
-        preNode = node;
-        node    = node->next;
-        free(preNode);
-    }
-    globalMsgs.first = NULL;
-}
+void msgsClean(void) { linkListClean(&globalMsgs); }
